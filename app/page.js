ca2acdb732bdc86336caf0ef19d5b3ae5bcd3e75
Greg <119018335+wkl761212@users.noreply.github.com>
2023-11-16 21:01:46 -0700

    test1

--- app/page.js
+++ app/page.js
@@ -5,6 +5,21 @@ import React, { useState, useEffect } from 'react';
 const Home = () => {
   // State to store the quote
   const [quote, setQuote] = useState({ q: '', a: '' });
+  const [options, setOptions] = useState([]);
+
+  const getRandomAuthors = (actualAuthor) => {
+    let randomAuthors = [];
+    const allAuthors = authorsData.authors.map(author => author.name);
+
+    while (randomAuthors.length < 2) {
+      const randomAuthor = allAuthors[Math.floor(Math.random() * allAuthors.length)];
+      if (randomAuthor !== actualAuthor && !randomAuthors.includes(randomAuthor)) {
+        randomAuthors.push(randomAuthor);
+      }
+    }
+    return randomAuthors;
+  };
+
   // Fetch data from the API
   async function getQuote() {
     try {
@@ -13,6 +28,8 @@ const Home = () => {
       if (data.length > 0) {
         // Assuming the first element in the array is the quote we need
         setQuote({ q: data[0].q, a: data[0].a });
+        const newOptions = getRandomAuthors(data[0].a);
+        setOptions([data[0].a, ...newOptions].sort(() => Math.random() - 0.5)); // Shuffle the options
       }
     } catch (error) {
       console.error("Error fetching quote: ", error);
@@ -32,18 +49,15 @@ const Home = () => {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">      
       <div>
         <p>{quote.q}</p>
-        <p></p>
       </div>
       <div>
-        <button  className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-2 px-4 rounded">option</button>
-        <button  className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-2 px-4 rounded">{quote.a}</button>
-        <button  className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-2 px-4 rounded">option</button>
+        {options.map((author, index) => (
+          <button key={index} className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-2 px-4 rounded">
+            {author}
+          </button>
+        ))}
       </div>
       <button onClick={handleNewQuote} className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-2 px-4 rounded">Next Quote</button>
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        
-      </div>
-
     </main>
   );
 };
