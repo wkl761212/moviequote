da605da79bffa7a8fc796f27b21c65f26f1d15c9
Greg <119018335+wkl761212@users.noreply.github.com>
2023-11-16 23:57:41 -0700

    11

--- app/page.js
+++ app/page.js
@@ -6,20 +6,20 @@ import React, { useState, useEffect } from 'react';
 const Home = () => {
   // State to store the quote
   const [quote, setQuote] = useState({ q: '', a: '' });
-  const [previousAuthor, setPreviousAuthor] = useState('');
   const [randomAuthors, setRandomAuthors] = useState([]);
 
   const getRandomAuthors = () => {
-    let authors = [];
+    let randomAuthors = [];
+    
     const allAuthors = authorsData.authors.map(author => author.name);
 
-    while (authors.length < 2) {
+    while (randomAuthors.length < 2) {
       const randomAuthor = allAuthors[Math.floor(Math.random() * allAuthors.length)];
-      if (!authors.includes(randomAuthor)) {
-        authors.push(randomAuthor);
+      if (!randomAuthors.includes(randomAuthor)) {
+        randomAuthors.push(randomAuthor);
       }
     }
-    return authors;
+    return randomAuthors;
   };
 
   // Fetch data from the API
@@ -29,7 +29,6 @@ const Home = () => {
       const data = await response.json();
       if (data.length > 0) {
         // Assuming the first element in the array is the quote we need
-        setPreviousAuthor(quote.a);
         setQuote({ q: data[0].q, a: data[0].a });
         setRandomAuthors(getRandomAuthors());
       }
@@ -51,22 +50,21 @@ const Home = () => {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <p>{quote.q}</p>
-        {previousAuthor && <p>Previous author: {previousAuthor}</p>} 
-      </div>
-      <div>
-        {randomAuthors.map((author, index) => (
-          <button key={index} className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-2 px-4 rounded my-2">
-            {author}
-          </button>
-        ))}
-      </div>
-      <button onClick={handleNewQuote} className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
-        Next Quote
-      </button>
-    </main>
-  );
+  <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div>
+      <p>{quote.q}</p>
+    </div>
+    <div>
+      {randomAuthors.map((author, index) => (
+        <button key={index} className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-2 px-4 rounded my-2">
+          {author}
+        </button>
+      ))}
+    </div>
+    <button onClick={handleNewQuote} className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+      Next Quote
+    </button>
+  </main>
+);
 };
 export default Home;
