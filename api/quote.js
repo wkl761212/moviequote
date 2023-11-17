3c478d1027c3e25cd9d09d0c703282189d85367b
Greg <119018335+wkl761212@users.noreply.github.com>
2023-11-16 18:13:31 -0700

    add 2 author

--- api/quote.js
+++ api/quote.js
@@ -1,7 +1,25 @@
-// api/quote.js
 module.exports = async (req, res) => {
-    const response = await fetch('https://zenquotes.io/api/random');
-    const data = await response.json();
-    res.status(200).json(data);
-  };
+ 
+  const response1 = await fetch('https://zenquotes.io/api/random');
+  const data1 = await response1.json();
+
+  let data2, data3;
+  do {
+   
+    const response2 = await fetch('https://zenquotes.io/api/random');
+    data2 = await response2.json();
+  } while (data1[0].a === data2[0].a);
+
+  do {
   
+    const response3 = await fetch('https://zenquotes.io/api/random');
+    data3 = await response3.json();
+  } while (data3[0].a === data1[0].a || data3[0].a === data2[0].a);
+
+
+  res.status(200).json({
+    quote: data1[0], 
+    randomAuthor1: data2[0].a,
+    randomAuthor2: data3[0].a
+  });
+};
--- app/page.js
+++ app/page.js
@@ -5,7 +5,8 @@ import React, { useState, useEffect } from 'react';
 const Home = () => {
   // State to store the quote
   const [quote, setQuote] = useState({ q: '', a: '' });
-  const [author, setAuthor] = useState({ a: '' });
+  const [randomAuthor1, setrandomAuthor1] = useState({ a: '' });
+  const [randomAuthor2, setrandomAuthor2] = useState({ a: '' });
   // Fetch data from the API
   async function getQuote() {
     try {
@@ -22,17 +23,29 @@ const Home = () => {
 
   async function getAuthor1() {
     try {
-      const response = await fetch('/api/quote');
-      const data = await response.json();
-      if (data.length > 0) {
+      const response2 = await fetch('/api/quote');
+      const data2 = await response2.json();
+      if (data2.length > 0) {
         // Assuming the first element in the array is the quote we need
-        setQuote({ a: data[0].a });
+        setQuote({ a: data2[0].a });
       }
     } catch (error) {
       console.error("Error fetching quote: ", error);
     }
   }
 
+  async function getAuthor2() {
+    try {
+      const response3 = await fetch('/api/quote');
+      const data3 = await response3.json();
+      if (data3.length > 0) {
+        // Assuming the first element in the array is the quote we need
+        setQuote({ a: data3[0].a });
+      }
+    } catch (error) {
+      console.error("Error fetching quote: ", error);
+    }
+  }
     
 
 // Use useEffect to fetch data when the component mounts
@@ -51,14 +64,12 @@ const Home = () => {
         <p>{quote.q}</p>
         <p>{quote.a}</p>
 
-        <p>{author.a}</p>
+        <p>{randomAuthor1.a}</p>
+
+        <p>{randomAuthor2.a}</p>
       </div>
       <button onClick={handleNewQuote} className="bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-2 px-4 rounded">Next Quote</button>
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <p className="absolute top-0 left-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Under Construction...
-        </p>
-      </div>
+      
 
     </main>
   );
