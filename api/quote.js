b413e78379c80ddbb0008a7894dae5df0461bb2b
Greg <119018335+wkl761212@users.noreply.github.com>
2023-11-22 23:11:23 -0700

    q

--- api/quote.js
+++ api/quote.js
@@ -1,12 +1,14 @@
-// api/quote.js
-// //module.exports = async (req, res) => {
-//     const response = await fetch('https://zenquotes.io/api/random');
-//     const data = await response.json();
-//     res.status(200).json(data);
-//   };
-  export default async function fetchQuote() {
-    const response = await fetch(`https://zenquotes.io/api/random`);
-    const data = await response.json();
-    return data || [];
-  };
+ api/quote.js
+ module.exports = async (req, res) => {
+     const response = await fetch('https://zenquotes.io/api/random');
+     const data = await response.json();
+     res.status(200).json(data);
+   };
+
+
+  //export default async function fetchQuote() {
+  //  const response = await fetch(`https://zenquotes.io/api/random`);
+   // const data = await response.json();
+   // return data || [];
+  //};
 
