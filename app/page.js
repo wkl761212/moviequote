b7dfdd995a09f801098d90471778a570cac4218a
Greg <119018335+wkl761212@users.noreply.github.com>
2023-11-26 15:19:58 -0700

    position update

--- app/page.js
+++ app/page.js
@@ -48,12 +48,12 @@ const Home = () => {
   
   return (
     <div style={{ backgroundColor: '#bfdbfe', minHeight: '100vh' }}>
-      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <main className="flex min-h-screen flex-col items-center justify-center pt-8">
         
         <div>
-          <p className="text-6xl text-black py-2 z-10">Who said this quote?</p>
+          <p className="text-6xl text-black p-1 m-1 z-10">Who said this quote?</p>
         </div>
-        <div className="relative w-full h-96 text-center"> {/* Adjust the height as needed */}
+        <div className="relative w-full h-96  m-6 text-center"> {/* Adjust the height as needed */}
           {/* The Image as Background */}
           <Image src="/parchment-bg.png" alt="Background" layout="fill" objectFit="contain" priority />
 
